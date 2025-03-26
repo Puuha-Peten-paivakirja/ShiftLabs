dff5294969981fc33caa939aa9f77236e1db3b6d
Ronja Ahonen <ronja.e.a.ahonen@gmail.com>
2025-03-26 04:46:00 +0200

    Make new group with user info

--- src/screens/GroupScreen.js
+++ src/screens/GroupScreen.js
@@ -1,13 +1,51 @@
 import React, { useState } from "react";
-import { View, StyleSheet, Text,TouchableOpacity } from "react-native";
+import { View, Button, StyleSheet, Text,TouchableOpacity } from "react-native";
 import Navbar from "../components/Navbar";
 import { TextInput } from "react-native-paper";
 import Ionicons from '@expo/vector-icons/Ionicons'
+import { addDoc, collection, firestore, GROUPS, GROUPUSERS, serverTimestamp, auth, USERS } from "../firebase/config.js";
 
 export default function GroupScreen() {
-  const [newName, setNewName] = useState('')
-  const [newDesc, setNewDesc] = useState('')
+  const [newGroup, setNewGroup] = useState({
+    groupName: '',
+    groupDesc: '',
+  })
 
+  const save = async () => {
+    console.log("Saving group:", newGroup);
+
+    const nowUser = auth.currentUser
+    if (nowUser !== null) {
+      const email = nowUser.email;
+
+      
+        
+      try{
+        const docRef = await addDoc(collection(firestore, GROUPS),{
+          groupName: newGroup.groupName,
+          description: newGroup.groupDesc,
+          created: serverTimestamp()
+        })
+        setNewGroup({ groupDesc:'', groupName:''})
+        console.log('New group saved')
+        
+        await addDoc(collection(firestore, GROUPS, docRef.id, GROUPUSERS), {
+          userId: nowUser.uid,
+          firstName: '',
+          email: email,
+          joined: serverTimestamp(),
+          role: "admin",
+        })
+        console.log('User added to gruop')
+
+      }catch (error) {
+        console.log("Error saving group or adding user:", error)
+      }
+
+  }
+  else{
+    console.log("You must log in first")
+  }}
 
   return (
     <View style={styles.container}>
@@ -24,11 +62,11 @@ export default function GroupScreen() {
           <TextInput
             style={styles.nameInput}
             placeholder="Ryhmän nimi..."
-            value={newName}
-            onChangeText={text => setNewName(text)}
+            value={newGroup.groupName}
+            onChangeText={text => setNewGroup({...newGroup, groupName:text})}
             numberOfLines={1}
           />
-          <TouchableOpacity style={styles.clearNameIcon} onPress={() => setNewName('')}>
+          <TouchableOpacity style={styles.clearNameIcon} onPress={() => setNewGroup({...newGroup, groupName:''})}>
             <Ionicons name='close-circle' size={20} />
           </TouchableOpacity>
         </View> 
@@ -38,14 +76,19 @@ export default function GroupScreen() {
           <TextInput
             style={styles.nameInput}
             placeholder="Ryhmän kuvaus..."
-            value={newDesc}
-            onChangeText={text => setNewDesc(text)}
+            value={newGroup.groupDesc}
+            onChangeText={text => setNewGroup({...newGroup, groupDesc:text})}
             numberOfLines={1}
           />
-          <TouchableOpacity style={styles.clearNameIcon} onPress={() => setNewDesc('')}>
+          <TouchableOpacity style={styles.clearNameIcon} onPress={() => setNewGroup({...newGroup, groupDesc:''})}>
             <Ionicons name='close-circle' size={20} />
           </TouchableOpacity>
         </View> 
+
+        <Button 
+          title='Luo'
+          onPress={save}
+        />
 
 
         
@@ -77,7 +120,7 @@ const styles = StyleSheet.create({
   },
   nameInput: {
     width: 280,
-    backgroundColor: '#FAF5FF', //change color!!!
+    backgroundColor: '#e6e0e9', //change color!!!
     borderBottomColor: 'black',
     borderBottomWidth: 0.8,
   },
