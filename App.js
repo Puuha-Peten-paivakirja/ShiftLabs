210b45b6a936216fcef56c0d42a24e6b2748d8ba
AapoKiiskila <aapo.kiiskila@gmail.com>
2025-03-28 11:55:30 +0200

    UserProvider to App.js and added a loading state for useAuth hook

--- App.js
+++ App.js
@@ -1,32 +1,35 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import HomeScreen from './src/screens/HomeScreen';
-import GroupScreen from './src/screens/GroupScreen';
-import WelcomeScreen from './src/screens/WelcomeScreen';
-import SettingsScreen from './src/screens/SettingsScreen';
-import AddShiftScreen from './src/screens/ShiftScreen';
-import ShiftScreen from './src/screens/ShiftScreen';
-import AllShiftsScreen from './src/screens/AllShiftsScreen';
+import HomeScreen from './src/screens/HomeScreen.js';
+import GroupScreen from './src/screens/GroupScreen.js';
+import WelcomeScreen from './src/screens/WelcomeScreen.js';
+import SettingsScreen from './src/screens/SettingsScreen.js';
+import AddShiftScreen from './src/screens/ShiftScreen.js';
+import ShiftScreen from './src/screens/ShiftScreen.js';
+import AllShiftsScreen from './src/screens/AllShiftsScreen.js';
 import SignUpScreen from './src/screens/SignUpScreen.js';
 import SignInScreen from './src/screens/SignInScreen.js';
+import { UserProvider } from './src/context/UserProvider.js';
 
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="Welcome" component={WelcomeScreen} />
-        <Stack.Screen name="SignUp" component={SignUpScreen} />
-        <Stack.Screen name="SignIn" component={SignInScreen} />
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Group" component={GroupScreen} />
-        <Stack.Screen name="Settings" component={SettingsScreen} />
-        <Stack.Screen name="AddShift" component={AddShiftScreen} />
-        <Stack.Screen name="Shift" component={ShiftScreen} />
-        <Stack.Screen name="AllShifts" component={AllShiftsScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <UserProvider>
+      <NavigationContainer>
+        <Stack.Navigator screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="Welcome" component={WelcomeScreen} />
+          <Stack.Screen name="SignUp" component={SignUpScreen} />
+          <Stack.Screen name="SignIn" component={SignInScreen} />
+          <Stack.Screen name="Home" component={HomeScreen} />
+          <Stack.Screen name="Group" component={GroupScreen} />
+          <Stack.Screen name="Settings" component={SettingsScreen} />
+          <Stack.Screen name="AddShift" component={AddShiftScreen} />
+          <Stack.Screen name="Shift" component={ShiftScreen} />
+          <Stack.Screen name="AllShifts" component={AllShiftsScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </UserProvider>
   );
 }
--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -3,19 +3,22 @@ import { auth, onAuthStateChanged } from '../firebase/config'
 
 export function useAuth() {
   const [user, setUser] = useState(null)
+  const [loading, setLoading] = useState(true)
 
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (user) => {
       if (user) {
         setUser(user)
+        setLoading(false)
       }
       else {
         setUser(null)
+        setLoading(false)
       }
     })
 
     return unsubscribe
   }, [])
 
-  return { user }
+  return { user, loading }
 }
