25433c892cbb1be718c5c93285205fb7b0ff6d70
Hikutaatti <niko.muikku618@gmail.com>
2025-04-02 09:27:08 +0300

    Deleted unused imports from app.js

--- App.js
+++ App.js
@@ -1,15 +1,6 @@
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import HomeScreen from './src/screens/HomeScreen';
-import GroupScreen from './src/screens/GroupScreen';
-import WelcomeScreen from './src/screens/WelcomeScreen';
-import SettingsScreen from './src/screens/SettingsScreen';
-import AddShiftScreen from './src/screens/AddShiftScreen';
-import ShiftScreen from './src/screens/ShiftScreen';
-import AllShiftsScreen from './src/screens/AllShiftsScreen';
-import SignUpScreen from './src/screens/SignUpScreen.js';
-import SignInScreen from './src/screens/SignInScreen.js';
+
 
 const Stack = createStackNavigator();
 import { UserProvider } from './src/context/UserProvider.js';
