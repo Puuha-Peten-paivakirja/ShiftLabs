8d13b64d72bf5b68caa590860e67744751243af2
AapoKiiskila <aapo.kiiskila@gmail.com>
2025-04-17 17:12:15 +0300

    Started implementing multi-language support

--- src/screens/WelcomeScreen.js
+++ src/screens/WelcomeScreen.js
@@ -4,25 +4,27 @@ import { Topbar } from '../components/Topbar.js'
 import { CustomButton } from '../components/CustomButton'
 import { useNavigation } from '@react-navigation/native'
 import styles from '../styles/Welcome.js'
+import {useTranslation} from 'react-i18next';
 
 export default function WelcomeScreen() {
   const navigation = useNavigation()
+  const {t} = useTranslation()
 
   return (
     <View style={styles.container}>
-      <Topbar title='Welcome'/>
+      <Topbar title={t('welcome')}/>
 
       <View style={styles.contentContainer}>
         <View></View>
         <View style={styles.buttonsContainer}>
-          <CustomButton title={'Sign up'} onPress={() => navigation.navigate('SignUp')} />
+          <CustomButton title={t('sign-up')} onPress={() => navigation.navigate('SignUp')} />
           <CustomButton
-            title={'Sign in'}
+            title={t('sign-in')}
             onPress={() => navigation.navigate('SignIn')} 
             style={{backgroundColor: '#d8bcfc'}}
           />
           <CustomButton
-            title={'Guest'}
+            title={t('guest')}
             onPress={() => navigation.navigate('Home')}
             style={{backgroundColor: '#d8bcfc'}}
           />        
